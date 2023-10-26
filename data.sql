4af7d54920ccb69ad67b6f71251370fee2c06cc2
jubaer919 <60255197+jubaer919@users.noreply.github.com>
2023-10-26 14:34:51 +0600

    Relation between TABLES and write some relational query

--- data.sql
+++ data.sql
@@ -22,3 +22,32 @@ VALUES
 (11, 'Ditto', '2022-05-14', 4, true, 22);
 
 
+/* day 3 project */
+
+INSERT INTO oweners(full_name, age)
+VALUES ('Sam Smith', 34),
+('Jennifer Orwell', 19),
+('Bob', 45),
+('Melody Pond', 77),
+('Dean Winchester', 14),
+('Jodie Whittaker', 34);
+
+
+INSERT INTO species(name) VALUES ('Pokemon'), ('Digimon');
+
+
+UPDATE animals SET species_id = 2 WHERE name LIKE '%mon';
+
+UPDATE animals SET species_id = COALESCE(species_id, 1) WHERE species_id IS NULL;
+
+UPDATE animals SET owner_id = 1 WHERE name = 'Agumon';
+
+UPDATE animals SET owner_id = 2 WHERE name = 'Gabumon' OR name = 'Pikachu';
+
+UPDATE animals SET owner_id = 3 WHERE name = 'Devimon' OR name = 'Plantmon';
+
+UPDATE animals SET owner_id = 4 WHERE name = 'Charmander' OR name = 'Squirtle' OR name = 'Blossom';
+
+UPDATE animals SET owner_id = 5 WHERE name = 'Angemon' OR name = 'Boarmon';
+
+
