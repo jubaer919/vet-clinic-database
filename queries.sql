fa01b0f2a0cddc0b9ae22f0843ed72a46e59c28f
jubaer919 <60255197+jubaer919@users.noreply.github.com>
2023-10-27 17:32:57 +0600

    Make join table and added some data and get some data from the table

--- queries.sql
+++ queries.sql
@@ -74,3 +74,23 @@ SELECT * FROM animals JOIN species ON animals.species_id = species.id JOIN owene
 SELECT * FROM animals JOIN oweners ON animals.owner_id = oweners.id JOIN species ON animals.species_id = species.id WHERE oweners.full_name ='Dean Winchester' AND animals.escape_attempts = 0;
 
 SELECT oweners.full_name, COUNT(animals.id) AS num_animals_owned FROM oweners LEFT JOIN animals ON oweners.id = animals.owner_id GROUP BY oweners.full_name ORDER BY num_animals_owned DESC LIMIT 1;
+
+/* day 4 project */
+
+SELECT animals.name, date_of_visits FROM visits JOIN animals ON visits.animals_id = animals.id WHERE visits.vets_id = (SELECT id FROM vets WHERE name = 'William Tatcher') ORDER BY visits.date_of_visits DESC LIMIT 1;
+
+SELECT DISTINCT(animals.name) FROM visits JOIN animals ON visits.animals_id = animals.id WHERE visits.vets_id = (SELECT id FROM vets WHERE name = 'Stephanie Mendez');
+
+SELECT vets.name, species.name FROM specializations LEFT JOIN vets ON specializations.vets_id = vets.id LEFT JOIN species ON specializations.species_id = species.id;
+
+SELECT animals.name FROM animals JOIN visits ON animals.id = visits.animals_id JOIN vets ON visits.vets_id = vets.id WHERE vets.name = 'Stephanie Mendez' and visits.date_of_visits BETWEEN '2020-04-01' AND '2020-08-30';
+
+SELECT animals.name, COUNT(visits.animals_id) AS count FROM animals JOIN visits ON animals.id = visits.animals_id GROUP BY animals.name ORDER BY count DESC LIMIT 1;
+
+SELECT animals.name FROM animals JOIN visits ON animals.id = visits.animals_id JOIN vets ON visits.vets_id = vets.id WHERE vets.name = 'Maisy Smith' ORDER BY visits.date_of_visits LIMIT 1;
+
+SELECT animals.name, visits.date_of_visits, vets.name FROM visits JOIN animals ON visits.animals_id = animals.id JOIN vets ON visits.vets_id = vets.id ORDER BY visits.date_of_visits DESC LIMIT 1;
+
+SELECT COUNT(*) FROM visits JOIN animals ON visits.animals_id = animals.id JOIN vets ON visits.vets_id = vets.id LEFT JOIN specializations s ON vets.id = s.vets_id AND animals.species_id = s.species_id WHERE s.vets_id IS NULL;
+
+SELECT s.name AS specialty_name, COUNT(*) AS animal_count FROM visits v JOIN animals a ON v.animals_id = a.id JOIN vets vet ON v.vets_id = vet.id JOIN specializations spec ON vet.id = spec.vets_id JOIN species s ON spec.species_id = s.id WHERE vet.name = 'Maisy Smith' GROUP BY s.name ORDER BY animal_count DESC LIMIT 1;
--- schema.sql
+++ schema.sql
@@ -31,3 +31,13 @@ ALTER TABLE animals DROP COLUMN species;
 ALTER TABLE animals ADD COLUMN species_id ADD CONSTRAINT fk_species_id FOREIGN KEY(species_id) REFERENCES species(id);
 
 ALTER TABLE animals ADD COLUMN owners_id ADD CONSTRAINT fk_owners_id FOREIGN KEY(owners_id) REFERENCES owners(id);
+
+
+/* day 4 project */
+
+CREATE TABLE vets (id BIGSERIAL NOT NULL PRIMARY KEY, name VARCHAR(100), age INT, date_of_graduation DATE);
+
+CREATE TABLE specializations(species_id BIGINT, vets_id BIGINT, PRIMARY KEY(species_id, vets_id), FOREIGN KEY (species_id) REFERENCES species(id), FOREIGN KEY (vets_id) REFERENCES vets (id));
+
+CREATE TABLE visits(animals_id BIGINT, vets_id BIGINT, date_of_visits DATE, PRIMARY KEY(animals_id, vets_id), FOREIGN KEY(animals_id) REFERENCES animals(id), FOREIGN KEY(vets_id) REFERENCES vets (id));
+
